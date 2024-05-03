32f5e3cf6a828d246da2dae7d417f9cb11cf8766
py3-dev <[200~yash.varshney@careers360.in>
2024-05-03 17:53:09 +0530

    setaup templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author" : "Yash Varshney",
+        "title" : "Blog Post 1",
+        "content" : "First post content",
+        "date_posted" : "May 03, 2024",
+        "date_updated" : "May 03, 2024"
+    },
+    {
+        "author" : "Shubh Varshney",
+        "title" : "Blog Post 2",
+        "content" : "Second post content",
+        "date_posted" : "May 01, 2024",
+        "date_updated" : "May 02, 2024"
+    }
+
+]
 
 def home(request):
-    return HttpResponse('<h1> Blog Welcome </h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog about </h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
