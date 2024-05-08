11ea2d79075414712419cf15073c357ca2383a01
py3-dev <[200~yash.varshney@careers360.in>
2024-05-08 06:25:46 +0530

    feat: post model

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="user_deleted")
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} written by {self.author} on {self.date_posted}"
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,8 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author" : "Yash Varshney",
-        "title" : "Blog Post 1",
-        "content" : "First post content",
-        "date_posted" : "May 03, 2024",
-        "date_updated" : "May 03, 2024"
-    },
-    {
-        "author" : "Shubh Varshney",
-        "title" : "Blog Post 2",
-        "content" : "Second post content",
-        "date_posted" : "May 01, 2024",
-        "date_updated" : "May 02, 2024"
-    }
-
-]
+from .models import Post
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts' : posts
     }
