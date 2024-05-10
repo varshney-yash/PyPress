bbe59c6a8d5936b47b9718648cbf4da96db8ccdf
py3-dev <[200~yash.varshney@careers360.in>
2024-05-10 12:37:43 +0530

    feat: register functionality

--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from . forms import UserRegForm
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form':form})
+    if request.method == 'POST':
+        form = UserRegForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created! Welcome, {username}")
+            return redirect('blog-home')
+        else:
+            for error in form.non_field_errors():
+                messages.error(request, error)
+            for field in form.errors:
+                for error in form.errors[field]:
+                    messages.error(request, error)
+    else:
+        form = UserRegForm()
+    return render(request, 'users/register.html', {'form' : form})
 
 def login(request):
     pass
