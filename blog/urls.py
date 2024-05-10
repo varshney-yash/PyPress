a16b526bc9915e5d86362ad0a0a0c41ef364ef34
py3-dev <[200~yash.varshney@careers360.in>
2024-05-11 00:46:54 +0530

    feat: filter by author

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>/', views.UserPostListView.as_view(), name='user-blogs'),
     path('blog/<int:pk>/', views.PostDetailView.as_view(), name='blog-detail'),
     path('blog/new/', views.PostCreateView.as_view(), name='blog-create'),
     path('blog/<int:pk>/update/', views.PostUpdateView.as_view(), name='blog-update'),
