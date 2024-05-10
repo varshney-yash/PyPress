a9a341948b8fd7c3e9fbb6c2902b3ee5ae0ef2af
py3-dev <[200~yash.varshney@careers360.in>
2024-05-10 15:38:12 +0530

    feat: profile updation

--- users/models.py
+++ users/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=200)
+    bio = models.TextField(max_length=200, null=True, blank=True, default="Hey, there! I'm writing on PyPress")
 
     def __str__(self) -> str:
         return f"{self.user.username}'s profile"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
