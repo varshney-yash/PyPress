79076a787e30eaed216b28ba00a350f730976f9e
py3-dev <[200~yash.varshney@careers360.in>
2024-05-11 01:08:03 +0530

    fix: safe

--- blog/templatetags/blog_filters.py
+++ blog/templatetags/blog_filters.py
@@ -8,4 +8,5 @@ def strip_html_and_truncate(value, arg):
     """Strips HTML tags and truncates to the specified number of characters."""
     stripped_html = strip_tags(value)
     truncated_text = (stripped_html[:arg] + '...') if len(stripped_html) > arg else stripped_html
+    print(truncated_text)
     return truncated_text
